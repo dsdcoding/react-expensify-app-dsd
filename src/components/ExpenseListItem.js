0d93a186b612c713bf0b3ffb6d5aec35f7488ea0
dsdcoding <ds44449999@gmail.com>
2021-01-03 03:56:35 -0500

    Formatting for Expenses amount and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 //Export a stateless functional component
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 //This is a stateless component but now it pulls data from the store
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
@@ -8,7 +10,11 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
         <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount / 100).format('$0,0.00')} 
+         -  
+        {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
 
     </div>
 );
