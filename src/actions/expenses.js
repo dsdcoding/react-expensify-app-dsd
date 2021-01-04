126c348c8fabb8a030e402f9dba7723ec052d23f
dsdcoding <ds44449999@gmail.com>
2021-01-04 02:25:03 -0500

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -20,14 +20,15 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
         const {
             description = '', 
             note = '', 
             amount = 0, 
             createdAt = 0} = expenseData;
             const expense = {description, note, amount, createdAt};      
-            database.ref('expenses').push(expense).then((ref) =>{
+            database.ref(`users/${uid}/expenses`).push(expense).then((ref) =>{
                 dispatch(addExpense({
                     id: ref.key,
                     ...expense
@@ -44,8 +45,9 @@ export const removeExpense = ({id} ={}) => ({
 });
 //asynchonize action below to get it going with firebase
 export const startRemoveExpense = ({id} = {}) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).remove().then(() => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${id}/expenses/${id}`).remove().then(() => {
             dispatch(removeExpense({id}));
         });
     };
@@ -65,8 +67,9 @@ export const editExpense = (id, updates) => ({
 
 //asynchonize action below to get it going with firebase..the RETURN is important or it doesnt work
 export const startEditExpense = (id, updates) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).update(updates).then(() => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${uid}`).update(updates).then(() => {
             dispatch(editExpense(id, updates));
         });
     };
@@ -88,8 +91,9 @@ export const setExpenses = (expenses) => ({
 
 //asynchonize action below to get it going with firebase
 export const startSetExpenses = () => {
-    return (dispatch) => {
-        return database.ref('expenses').once('value').then((snapshot) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
             const expenses = [];
 
             snapshot.forEach((childSnapshot) => {
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,8 +7,8 @@ import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import NotFoundPage from '../components/NotFoundPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
+import PublicRoute from './PublicRoute';
 
 //helps tell the urls you must be authenticated to see it
 import PrivateRoute from './PrivateRoute';
@@ -20,11 +20,10 @@ const AppRouter = () => (
     <Router history ={history}>
     <div>
         <Switch>
-        <Route path="/" component={LoginPage} exact={true}/>
+        <PublicRoute path="/" component={LoginPage} exact={true}/>
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
         <PrivateRoute path="/create" component={AddExpensePage}/>
         <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
         <Route  component={NotFoundPage}/>
 
         </Switch>
